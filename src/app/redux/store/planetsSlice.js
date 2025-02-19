05bbac1f945a962af83d1e8d7893806c41ebaf2e
himanshu1000 <himanshu.baghel@blockstack.tech>
2025-02-19 21:49:39 +0530

    feat: Redux setup

---
+++ src/app/redux/store/planetsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const planetSlice = createSlice({
+  name: "planets",
+  initialState: {
+    planets: [],
+    filters: {
+      color: [],
+      shape: [],
+      size: [],
+    },
+    searchText: "",
+  },
+  reducers: {
+    setPlanets(state, action) {
+      state.planets = action.payload;
+    },
+    setFilters(state, action) {
+      state.filters = action.payload;
+    },
+    setSearchText(state, action) {
+      state.searchText = action.payload;
+    },
+  },
+});
+
+export const { setPlanets, setFilters, searchText } = planetSlice.actions;
+export default planetSlice.reducer;
