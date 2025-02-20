418cef15c20c0dbf11f76f17bd2c63a038ac0767
himanshu1000 <himanshu.baghel@blockstack.tech>
2025-02-20 22:09:28 +0530

    Add: Added Ant patch package and updated Providers file name in Layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./redux/store/provider";
+import { Providers } from "./redux/store/Provider";
+import "@ant-design/v5-patch-for-react-19";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
