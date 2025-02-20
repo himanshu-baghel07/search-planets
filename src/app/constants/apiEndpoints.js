1f9e1ca40a61b34e5fd254730799d7d4b1525135
himanshu1000 <himanshu.baghel@blockstack.tech>
2025-02-20 22:14:45 +0530

    Add: Added constants for API endpoints and base URL

---
+++ src/app/constants/apiEndpoints.js
@@ -0,0 +1,10 @@
+// Base URL for the API
+export const BASE_URL = "https://api-planets.vercel.app";
+
+// URI constants for different API endpoints
+export const URI = {
+  PLANETS: `${BASE_URL}/planets`,
+  COLORS: `${BASE_URL}/colors`,
+  SHAPES: `${BASE_URL}/shapes`,
+  SIZES: `${BASE_URL}/sizes`,
+};
