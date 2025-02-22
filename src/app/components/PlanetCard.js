1c198d70a2a457fce5c8e94c4b9e6a711db26925
himanshu1000 <himanshu.baghel@blockstack.tech>
2025-02-22 20:32:45 +0530

    Add: Added method for generating description for PlanetCard component

--- src/app/components/PlanetCard.js
+++ src/app/components/PlanetCard.js
@@ -1,12 +1,24 @@
 import { Card, Typography } from "antd";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const PlanetCard = ({ planet }) => {
+  const { description } = useSelector((state) => state.planets);
+
+  const generateDescription = (planet, description) => {
+    const colorName = description.color[planet.color]?.name || "unknown color";
+    const shapeName = description.shape[planet.shape]?.name || "unknown shape";
+    const sizeName = description.size[planet.size]?.name || "unknown size";
+
+    return `${planet.name} ${colorName} color, ${shapeName} shape, and ${sizeName} size`;
+  };
+
+  const formattedDescription = generateDescription(planet, description);
   const { Title, Text } = Typography;
   return (
     <Card hoverable className="mb-4 fade-in">
       <Title level={4}>{planet.name}</Title>
-      <Text type="secondary">{planet.description}</Text>
+      <Text type="secondary">{formattedDescription}</Text>
     </Card>
   );
 };
