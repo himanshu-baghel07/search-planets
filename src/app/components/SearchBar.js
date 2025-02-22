c086cc7081bd9c157f3fe0ab89dbb76bcce3d06d
himanshu1000 <himanshu.baghel@blockstack.tech>
2025-02-22 20:31:31 +0530

    Add: Added search button styling for SearchBar component

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -10,6 +10,11 @@ const SearchBar = ({ value, onSearch, onChange }) => {
       onChange={onChange}
       size="large"
       className="mb-6"
+      enterButton
+      style={{
+        "--ant-primary-color": "#1890ff", // Ant Design's default blue
+        "--ant-primary-color-hover": "#40a9ff",
+      }}
     />
   );
 };
